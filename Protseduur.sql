13d78d240480c82be1014aa23e2e34f1ebbe4f7e
Milan Petrovski <milan.petrovski8@gmail.com>
2025-02-04 13:54:56 +0200

    Update Protseduur.sql

--- Protseduur.sql
+++ Protseduur.sql
@@ -2,7 +2,9 @@
 --SQL SALVESTATUD PROTSEDUUR funktsioon, mille saate salvestada, nii et koodi saab ikka ja jälle kasutada. / funktsioon mis käivitav serveris mitu SQL tegevust järjest.
 
 --Kasutame SQL server
-
+--############################################
+--###############/////SQL/////################
+--############################################
 
 CREATE DATABASE ProtseduurMelon3000
 
@@ -67,8 +69,35 @@ CREATE PROCEDURE LinnaOtsing
 AS 
 BEGIN
 SELECT * FROM Linn
-WHERE linnNIMI LIKE @taht + '%'; --------------------------------------------------------------------------------------------------------------!!!!!!!
+WHERE linnNIMI LIKE @taht + '%';
 --% - kõik teised tähted
 END;
 --kutse
 EXEC LinnaOtsing '%'
+
+
+--KASUTAME XAMPP
+--############################################
+--##############/////XAMPP/////###############
+--############################################
+
+CREATE TABLE Linn
+(
+linnID INT PRIMARY KEY AUTO_INCREMENT,
+linnNIMI VARCHAR(30), 
+rahvaARV INT
+);
+
+INSERT INTO Linn(linnNIMI, rahvaARV)
+VALUES
+('TALLINN', 437980),
+('MOSCOW', 11630060),
+('PRAGUE',1980231);
+
+CREATE PROCEDURE `lisaLinn`(IN `lnimi` VARCHAR(30), IN `rarv` INT) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN
+INSERT INTO Linn(linnNIMI, rahvaARV)
+VALUES (lnimi, rarv);
+SELECT * FROM Linn;
+END;
+
+CALL lisaLinn ('NARVA', 255555);
