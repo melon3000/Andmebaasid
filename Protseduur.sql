a9dce68adf1a20d350f52148daaec7401cd99e9d
Milan Petrovski <milan.petrovski8@gmail.com>
2025-02-04 12:52:57 +0200

    Create Protseduur.sql

---
+++ Protseduur.sql
@@ -0,0 +1,2 @@
+(localdb)/mssqllocaldb
+--SQL SALVESTATUD PROTSEDUUR funktsioon, mille saate salvestada, nii et koodi saab ikka ja jälle kasutada. / funktsioon mis käivitav serveris mitu SQL tegevust järjest.
